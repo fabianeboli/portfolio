fe3b26161599c0361cdd68f8a7c91e18471f4611
Fabian Eboli <fabianeboli@pm.me>
2024-06-18 01:53:50 +0200

    Config app

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,7 @@ export default defineAppConfig({
   ui: {
     primary: 'bg-emerald-700 dark:bg-emerald-500',
     gray: 'bg-gray-50 dark:bg-gray-800',
-  }
+  },
+  nuxtIcon: {},
+  nuxtMail: {},
 })
